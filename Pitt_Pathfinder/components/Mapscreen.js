6c558ed5ed315951e281e58e002b95f10742d661
jweir136 <jweir136@yahoo.com>
2022-01-04 15:57:34 -0500

    add state management for current location

--- Pitt_Pathfinder/components/Mapscreen.js
+++ Pitt_Pathfinder/components/Mapscreen.js
@@ -4,11 +4,21 @@ import { View, Text, Platform } from 'react-native';
 import Geolocation, { requestAuthorization } from 'react-native-geolocation-service';
 
 export default class Mapscreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'lat':0,
+            'lon':0
+        };
+    }
+
     componentDidMount() {
         Geolocation.requestAuthorization('whenInUse');
         let _watchId = Geolocation.watchPosition(
             (position) => {
-                console.log(position);
+                this.state['lat'] = position.coords.latitude;
+                this.state['lon'] = position.coords.longitude;
+                console.log(this.state);
             },
             (error) => {
                 console.log(error.code, error.message);
