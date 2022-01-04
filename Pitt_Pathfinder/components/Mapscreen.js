a7dc111e6efa4368ef9d12dc2f537ad378f4ebb5
jweir136 <jweir136@yahoo.com>
2022-01-04 15:02:19 -0500

    get persmissions working for geolocation services

--- Pitt_Pathfinder/components/Mapscreen.js
+++ Pitt_Pathfinder/components/Mapscreen.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
-const Mapscreen = ({ navigation }) => {
-    return (
-      <Text>Mapscreen</Text>
-    );
-  };
+import Geolocation, { requestAuthorization } from 'react-native-geolocation-service';
 
-export default Mapscreen;
+export default class Mapscreen extends Component {
+    componentDidMount() {
+        Geolocation.requestAuthorization('whenInUse');
+        let _watchId = Geolocation.watchPosition(
+            (position) => {
+                console.log(position);
+            },
+            (error) => {
+                console.log(error.code, error.message);
+            },
+            {
+                enabledHighAccuracy: true,
+                timeout: 15000,
+                maximumAge: 10000    
+            }
+        );
+    }
+
+    render() {
+        return <Text>Hello, World!</Text>
+    }
+}
