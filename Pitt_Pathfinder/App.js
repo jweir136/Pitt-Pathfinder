6673d82ec97743406f0b048015a1195106dbaa55
jweir136 <jweir136@yahoo.com>
2022-01-04 12:04:51 -0500

    add first outline of Mapscreen navigation

--- Pitt_Pathfinder/App.js
+++ Pitt_Pathfinder/App.js
@@ -1,10 +1,22 @@
 import * as React from 'react';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Mapscreen from './components/Mapscreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* Rest of your app code */}
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Map"
+          component={Mapscreen}
+          options={{ title: 'Map' }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
--- Pitt_Pathfinder/components/Mapscreen.js
+++ Pitt_Pathfinder/components/Mapscreen.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class Mapscreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Mapscreen</Text>
-            </View>
-        );
-    }
-}
+const Mapscreen = ({ navigation }) => {
+    return (
+      <Text>Mapscreen</Text>
+    );
+  };
+
+export default Mapscreen;
